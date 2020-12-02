03026a819db2421e36e8b4d42dbaacce8d977124
Stewart McKinlay <stewart.mckinlay@gmail.com>
2020-12-02 14:38:06 +0000

    created input module and refactored code

--- client.js
+++ client.js
@@ -13,31 +13,11 @@ const connect = () => {
     conn.write("Name: SM");
   });
 
-  conn.on('connect', () => {
-    setupInput();
-  });
-
   conn.on('data', (data) => {
     console.log('SERVER: ', data);
   });
 
   return conn;
-};
-
-const setupInput = () => {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding('utf8');
-  stdin.resume();
-  stdin.on("data", (data) => {
-    handleUserInput(data);
-    return stdin;
-  });
-};
-
-const handleUserInput = (data) => {
-  if (data === '\u0003')
-    process.exit();
 };
 
 module.exports = connect;
