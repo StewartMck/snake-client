34b588a08d7f7d8d57f093525b04091496f7dbcb
Stewart McKinlay <stewart.mckinlay@gmail.com>
2020-12-02 16:20:27 +0000

    input commands now sent to server

--- input.js
+++ input.js
@@ -1,6 +1,8 @@
 const { builtinModules } = require("module");
+let connection;
 
-const setupInput = () => {
+const setupInput = (conn) => {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding('utf8');
@@ -14,6 +16,21 @@ const setupInput = () => {
 const handleUserInput = (data) => {
   if (data === '\u0003')
     process.exit();
+
+  switch (data) {
+  case 'w':
+    connection.write("Move: up");
+    break;
+  case 'a':
+    connection.write("Move: left");
+    break;
+  case 's':
+    connection.write("Move: down");
+    break;
+  case 'd':
+    connection.write("Move: right");
+    break;
+  }
 };
 
 module.exports = {
