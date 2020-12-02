ab07d796c34db3ca1efa9321f2e53d64811ac347
Stewart McKinlay <stewart.mckinlay@gmail.com>
2020-12-02 13:20:40 +0000

    created client with code from play - modularized

--- play.js
+++ play.js
@@ -1,20 +1,5 @@
-const net = require('net');
 
-const connect = () => {
-
-  const conn = net.createConnection({
-    host: 'localhost',
-    port: 50541
-  });
-  conn.setEncoding('utf8');
-
-  conn.on('data', (data)=>{
-    console.log('SERVER: ', data);
-  });
-
-  return conn;
-};
-
+const connect = require('./client');
 console.log('Connecting...');
 connect();
 
